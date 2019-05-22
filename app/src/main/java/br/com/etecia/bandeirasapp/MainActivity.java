46a4babfbfea88982d9a385906a0369b89b5ea64
daviidelucas <davi.franca2@etec.sp.gov.br>
2019-05-21 22:03:22 -0300

    upload project

---
+++ app/src/main/java/br/com/etecia/bandeirasapp/MainActivity.java
@@ -0,0 +1,24 @@
+package br.com.etecia.bandeirasapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.lang.reflect.Array;
+
+import br.com.etecia.bandeirasapp.model.State;
+
+public class MainActivity extends AppCompatActivity {
+
+    State states [] = {new State("","", "", "")};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+
+}
